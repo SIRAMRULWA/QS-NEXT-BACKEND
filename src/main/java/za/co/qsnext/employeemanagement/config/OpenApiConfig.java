510d1332104821de282a8bf581d34f9d25b09259
SIRAMRULWA <sihlemrulwa@gmail.com>
2026-09-04 08:34:06 +0200

    Document JWT bearer authentication in OpenAPI

--- src/main/java/za/co/qsnext/employeemanagement/config/OpenApiConfig.java
+++ src/main/java/za/co/qsnext/employeemanagement/config/OpenApiConfig.java
@@ -1,4 +1,44 @@
 package za.co.qsnext.employeemanagement.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI employeeManagementOpenAPI() {
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("QSNext Employee Management System API")
+                                .version("v1")
+                                .description(
+                                        "REST API for the QSNext Employee Management System"
+                                )
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        SECURITY_SCHEME_NAME,
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(SECURITY_SCHEME_NAME)
+                );
+    }
 }
